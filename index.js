b79cb9baba6949c9a20d96052092f58a7a29632c
Misko14 <76009536+Misko14@users.noreply.github.com>
2022-08-01 12:08:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once("ready", () => {
-  console.log("Misk Test Bot enabled!");
+  console.log("Misk Ping Bot enabled!");
 });
 
 // To start pinging, say the Start Ping Command in any channel.
