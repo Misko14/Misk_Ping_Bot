5814dc6e19e6762ff6e45878fe3fe1929fba0cbe
Misko14 <76009536+Misko14@users.noreply.github.com>
2022-08-09 16:27:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 // Settings
 // -----------------------------------
+var ServerID = "1000149595000680548"
 var PingRoleID = "1000150853367373845"
 var PingChannelPrefix = "pings"
 var PingInterval = 2000 // Milliseconds (1 Second = 1000 Milliseconds)
 // -----------------------------------
 
 client.once("ready", () => {
+  client.guilds.cache.forEach(
+    function(server) {
+      if (server.id !== ServerID) {
+        server.leave()
+      }
+    })
   setInterval(function() {
     client.channels.cache.forEach(
       function(channel) {
